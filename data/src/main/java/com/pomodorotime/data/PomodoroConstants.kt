a97f86b5efe24748058afec27e6ca1c3e1789a47
Roberto <rmp0046@gmail.com>
2020-09-29 17:37:50 -0300

    Added constants

--- data/src/main/java/com/pomodorotime/data/PomodoroConstants.kt
+++ data/src/main/java/com/pomodorotime/data/PomodoroConstants.kt
@@ -1,2 +1,5 @@
 package com.pomodorotime.data
 
+const val POMODORO_DEFAULT_TIME: Long = 25 * 1000
+const val POMODORO_SMALL_BREAK_DEFAULT_TIME: Long = 5 * 1000
+const val POMODORO_LONG_BREAK_DEFAULT_TIME: Long = 15 * 1000
