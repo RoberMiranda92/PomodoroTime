3fe30d128cf031df53e7e472d5a1c43a73d2093a
Roberto <rmp0046@gmail.com>
2021-01-12 13:43:44 -0300

    commit 1 branch 3

--- domain/src/main/java/com/pomodorotime/domain/models/ErrorEntity.kt
+++ domain/src/main/java/com/pomodorotime/domain/models/ErrorEntity.kt
@@ -4,8 +4,6 @@ sealed class ErrorEntity {
     data class GenericError(val code: Int? = null, val message: String) : ErrorEntity()
     data class UserEmailError(val message: String) : ErrorEntity()
     data class UserPasswordError(val message: String) : ErrorEntity()
-
-
     
     object NetworkError : ErrorEntity()
 }
